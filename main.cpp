b363e4a379dc6a9af637c7229debc98edbfc57f8
UlisesAceves <ulises.aceves7480@alumnos.udg.mx>
2020-09-23 19:34:25 -0500

    Dialogo basico en funcionamiento

--- main.cpp
+++ main.cpp
@@ -12,7 +12,17 @@ int main() {
     cout << "Y dime que edad tienes? "<<endl;
     cin>>edad;
     cout << "Asi que en 5 años tendras: "<<edad+5<<" Interesante"<<endl;
-	return 0;
+    system("pause");
+    system("cls");
+	cout << "Pero donde estan mis modales? Permiteme presentarme soy main 2.0 es un placer conocerte."<<endl;
+    system("pause");
+    system("cls");
+    cout << "Hum? Que que puedo hacer, pues de momento no mucho, la verdad es que aun sigo en desarrollo, solo tengo dos funciones las cuales ni siquiera estan completas"<<endl;
+    system("pause");
+    system("cls");
+    cout << "bueno tengo que irme antes de que descubran que me escape. Cuidate";
+
+    return 0;
 }
 
 
