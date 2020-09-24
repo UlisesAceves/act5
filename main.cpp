998277f268a5c0ee232d6cfa00bc817dba73f7f3
UlisesAceves <ulises.aceves7480@alumnos.udg.mx>
2020-09-23 19:19:13 -0500

    Pedir y mostrar el nombre funcionando

--- main.cpp
+++ main.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 
 int main() {
+    char nombre[50];
+    int edad;
 	cout << "Hello World!" << endl;
+    cout << "Cual es tu  nombre? ";
+    fgets(nombre,sizeof(nombre),stdin);
+    cout << "Asi que te llamas: "<<nombre<<endl;
 
 	return 0;
 }
