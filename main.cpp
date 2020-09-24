08a74b90dc02aabe4a0cab2808eaffaf5e4a49c7
UlisesAceves <ulises.aceves7480@alumnos.udg.mx>
2020-09-23 19:24:10 -0500

    Pedir y mostrar la edad+5 funcional

--- main.cpp
+++ main.cpp
@@ -6,10 +6,13 @@ int main() {
     char nombre[50];
     int edad;
 	cout << "Hello World!" << endl;
-    cout << "Cual es tu  nombre? ";
+    cout << "Cual es tu  nombre? "<<endl;
     fgets(nombre,sizeof(nombre),stdin);
     cout << "Asi que te llamas: "<<nombre<<endl;
-
+    cout << "Y dime que edad tienes? "<<endl;
+    cin>>edad;
+    cout << "Asi que en 5 años tendras: "<<edad+5<<" Interesante"<<endl;
 	return 0;
 }
+
 
